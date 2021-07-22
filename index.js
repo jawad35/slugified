16d923ef2eaf3d173a3c132b787027b6e96b0194
jawad ali <024jawadali@gmail.com>
2021-07-22 16:52:13 +0500

    first comment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function slugified(str, venue) {
-  str = str.replace(/^\s+|\s+$/g, ""); // trim
-  str = str.toLowerCase();
+function slugified(town, venue) {
+  town = town.replace(/^\s+|\s+$/g, ""); // trim
+  town = town.toLowerCase();
   venue = venue.replace(/^\s+|\s+$/g, ""); // trim
   venue = venue.toLowerCase();
 
@@ -9,13 +9,13 @@ function slugified(str, venue) {
   var to = "aaaaaeeeeiiiioooouuuunc------";
 
   for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+    town = town.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
   }
   for (var i = 0, l = from.length; i < l; i++) {
     venue = venue.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
   }
 
-  str = str
+  town = town
     .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
     .replace(/\s+/g, "-") // collapse whitespace and replace by -
     .replace(/-+/g, "-"); // collapse dashes
@@ -23,9 +23,9 @@ function slugified(str, venue) {
     .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
     .replace(/\s+/g, "-") // collapse whitespace and replace by -
     .replace(/-+/g, "-"); // collapse dashes
-  return "/" + str + "-event-/" + venue;
+  return "/" + town + "-event/" + venue;
 }
 
-// const string = slugified('Bristol','Thekla');
+// const string = slugified("Bristol", "Thekla");
 // console.log(string);
 // /bristol-events/thekla
